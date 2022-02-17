44477a7e92c3463b97cefb9f9d23327adedf1098
Larissa Bandeira <lbesouza@rd.com.br>
2022-02-17 16:34:10 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import './App.css';
+import React from 'react';
+import Primeiro from './components/Primeiro';
+import CompPadrao, {Component1, Component2} from './components/Multi.js';
 
 function App() {
-  
+  return (<Primeiro></Primeiro>);
 }
+
+
 
 export default App;
