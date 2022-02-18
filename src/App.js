f4d84a3f84028f663626527e03b0f9703f009139
Larissa Bandeira <lbesouza@rd.com.br>
2022-02-18 11:31:03 -0300

    adicionando botao

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import Primeiro from './components/Primeiro';
 import CompPadrao, {Component1, Component2} from './components/Multi.js';
+import Estilos from './components/Estilos';
+import MinMax from './components/MinMax';
+import Titulo from './components/Titulo';
+import Botao from './components/Botao';
 
 function App() {
-  return (<Primeiro></Primeiro>);
+  return (
+    <div>
+      <Primeiro />
+      <CompPadrao />
+      <Component1 />
+      <Component2 />
+      <Estilos />
+      <MinMax min="2" max="10" />
+      <MinMax min={4} max={15} />
+      <Titulo principal="Olá" secudanrio="tudo bem" />
+      <Botao />
+  </div>
+  );
 }
 
 
